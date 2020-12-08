662cc75afe276ded878365a91dd463f7acb9a778
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2020-12-08 23:20:21 +0200

    alignment corrections

--- script/script.js
+++ script/script.js
@@ -2,14 +2,6 @@ let openBtn = document.getElementById('open');
 let closeBtn = document.querySelector('.closebtn');
 let closeBtns = document.querySelectorAll('.nav-button');
 
-// for (let i = 0; i < closeBtns.length; i++) {
-//   let button = closeBtns[i];
-//   button.addEventListener('click', event => {
-//     event.preventDefault();
-//     document.getElementById("navigation").style.width = "0";
-//   })
-// }
-
 closeBtns.forEach(btn => {
   btn.addEventListener('click', event => {
     event.preventDefault();
@@ -20,7 +12,7 @@ closeBtns.forEach(btn => {
 openBtn.addEventListener('click', event => {
   event.preventDefault();
   console.log('test');
-  document.getElementById("navigation").style.width = "180px";
+  document.getElementById("navigation").style.width = "170px";
 });
 
 closeBtn.addEventListener('click', event => {
